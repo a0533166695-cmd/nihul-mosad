b9275266dc115b96e70beba047b4be0d68fe7962
a0533166695-cmd <a0533166695@gmail.com>
2026-09-24 01:40:49 +0300

    עדכון sw.js

---
+++ sw.js
@@ -0,0 +1,13 @@
+const C='tt-app-v1';
+const CORE=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/maskable-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(CORE)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{
+  const r=e.request;if(r.method!=='GET')return;
+  const u=new URL(r.url);
+  if(r.mode==='navigate'){ // עמוד: קודם רשת (לעדכונים), ואם אין אינטרנט - מהזיכרון
+    e.respondWith(fetch(r).then(res=>{const cp=res.clone();caches.open(C).then(c=>c.put('./index.html',cp));return res;}).catch(()=>caches.match('./index.html')));return;}
+  e.respondWith(caches.match(r).then(hit=>hit||fetch(r).then(res=>{
+    if(res.ok&&(u.origin===location.origin||/fonts\.(googleapis|gstatic)\.com|cdnjs|jsdelivr/.test(u.host))){const cp=res.clone();caches.open(C).then(c=>c.put(r,cp));}
+    return res;}).catch(()=>hit)));
+});

bdb73366c7ccf78f4e47572037308ceef9c9fb03
nihul-mosad <a0533166695@gmail.com>
2026-09-24 23:31:17 +0300

    עדכון sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='tt-app-v1';
+const C='tt-app-v2';
 const CORE=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/maskable-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(CORE)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim()));});
